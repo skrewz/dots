a929d6dddb45fef04265d8b71420d215d1f5573c
Anders Breindahl <anders.breindahl@booking.com>
2014-09-04 11:05:57 +0200

    Move to scripts/symlinks for this.

--- awesome/support_scripts/skrewz-left-scratch.sh
+++ awesome/support_scripts/skrewz-left-scratch.sh
@@ -1,4 +1,1 @@
-#!/bin/bash
-
-#/usr/bin/urxvt -background rgba:0000/0000/0000/F000 -e bash -ic 'ssh -t skrewz@slovener.hosts.skrewz.net screen -xRR weechat'
-/usr/bin/urxvt -background rgba:0000/0000/0000/F000 -e bash -ic 'screen -xRR'
+skrewz-local-screen-scratch.sh
