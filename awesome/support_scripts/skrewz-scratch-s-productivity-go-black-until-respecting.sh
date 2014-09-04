a1034c0b694781f1db2765800007a7893ab8ab14
Anders Breindahl <anders.breindahl@booking.com>
2014-09-04 11:46:17 +0200

    Pretty output to dismiss myself.

--- awesome/support_scripts/skrewz-scratch-s-productivity-go-black-until-respecting.sh
+++ awesome/support_scripts/skrewz-scratch-s-productivity-go-black-until-respecting.sh
@@ -3,7 +3,7 @@
 # Check if the kill file exists, and if it does (and is still relevant), noop out:
 if [ -e /tmp/s-productivity-go-black-until ] && (( $(date +%s) < $(stat -c%Y /tmp/s-productivity-go-black-until) )); then
   screen -D # to kill the previous scratch window here.
-  /usr/bin/urxvt -background rgba:0000/0000/0000/F000 -e bash -ic "echo 'You are not supposed to be here.'; echo '/tmp/s-productivity-go-black-until expires at: $(date -d @$(stat -c%Y /tmp/s-productivity-go-black-until) +%F\ %T).'; sleep 2"
+  /usr/bin/urxvt -background rgba:FFFF/0000/0000/3000 -fn '-*-terminus-*-*-*-*-*-320-*-*-*-*-*-*' -e bash -ic $'echo "\n\n\e[41mYou are not supposed to be here.\n\n\e[0m"; echo "It is now:\n\n    '"$(date +%F\ %T)"$'\n\n/tmp/s-productivity-go-black-until expires:\n\n    '"$(date -d @$(stat -c%Y /tmp/s-productivity-go-black-until) +%F\ %T) (>=$(( ($(date -d @$(stat -c%Y /tmp/s-productivity-go-black-until) +%s)-$(date +%s))/3600 ))h away)"$'.\n\n"; for((i=10;--i;i>0)); do echo -n "\rQuitting in... $i"; sleep 1; done'
   exit 0
 fi
 
