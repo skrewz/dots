f87a0cf4ebbc83941cf6e143c1f332c116b667fb
Anders Breindahl <anders.breindahl@thinkbiganalytics.com>
2016-10-18 21:20:53 +0200

    awesome: screen attach on litauer.

--- awesome/support_scripts/skrewz-local-screen-scratch.sh
+++ awesome/support_scripts/skrewz-local-screen-scratch.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#/usr/bin/urxvt -background rgba:0000/0000/0000/F000 -e bash -ic 'ssh -t skrewz@slovener.hosts.skrewz.net screen -xRR weechat'
 /usr/bin/urxvt -background rgba:0000/0000/0000/F000 -e bash -ic 'screen -xRR'
