a4c2b6dba786b923d0e6c80807bbe1bb98328e83
Anders Breindahl <skrewz@skrewz.net>
2018-05-04 22:10:32 +0200

    Bash: moving prompt highlighting around.

--- .bash/.bashrc.skrewz.d/privsideprompt.sh
+++ .bash/.bashrc.skrewz.d/privsideprompt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-if [ "773a2038b8bfe5ca064e8de03079e711  -" = "$(hostname -s | md5sum)" ]; then
+
+if hostname -s | md5sum | grep -qF $'773a2038b8bfe5ca064e8de03079e711\n33c4ca47bb83c1b193620757dd9a7231'; then
   functions_to_render_prompt+=( render_no_ssh_profiles )
 fi
