8dc855fd5acad7d8e119f056a5c6d67d3377543d
Anders Breindahl <skrewz@skrewz.net>
2024-01-22 21:56:41 +1100

    bugfix: arbitrate urgent window if more than one

--- sway/scripts/select-urgent-container.sh
+++ sway/scripts/select-urgent-container.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-any_urgent_id=$(swaymsg -t get_tree| jq -r 'recurse(.nodes[]?) | recurse(.floating_nodes[]?) | select(.type=="con"), select(.type=="floating_con") | select(.urgent==true) | {id, app_id, name} | (.id|tostring)')
+any_urgent_id=$(swaymsg -t get_tree| jq -r '[recurse(.nodes[]?) | recurse(.floating_nodes[]?) | select(.type=="con"), select(.type=="floating_con") | select(.urgent==true) | {id, app_id, name}] | .[0] | (.id|tostring)')
 
 swaymsg "[con_id=$any_urgent_id]" focus
