1df854f191e9b3e8833c19d11a2a5ea0faa41a55
Anders Breindahl <skrewz@skrewz.net>
2024-09-23 19:59:46 +1000

    ✨ Add (mostly) cmp-related config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,12 +5,12 @@ vim.opt.autoread = true
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
 vim.opt.dir=os.getenv("HOME") .. "/.vim_local/swapfiles"
-vim.opt.guifont = "Hack:h18"
+vim.opt.guifont = "Hack:h8"
 vim.opt.hidden = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.list = true
-vim.opt.listchars = "tab:  ,trail: ,extends:·"
+vim.opt.listchars = { leadmultispace = "◦···", tab = "⇥ " }
 vim.opt.modeline = true
 vim.opt.scroll=10
 vim.opt.shiftwidth=2
@@ -251,6 +251,87 @@ require('lualine').setup{
 }
 
 require('ibl').setup()
+
+local cmp = require'cmp'
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    end,
+  },
+  mapping = {
+    ['<S-s>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    { name = 'gitmoji' },
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities() --nvim-cmp
+
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+end
+
+-- Setup lspconfig.
+local nvim_lsp = require('lspconfig')
+
+-- setup languages 
+-- GoLang
+nvim_lsp['gopls'].setup{
+  cmd = {'gopls'},
+  -- on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    gopls = {
+      experimentalPostfixCompletions = true,
+      analyses = {
+        unusedparams = true,
+        shadow = true,
+      },
+      staticcheck = true,
+    },
+  },
+  init_options = {
+    usePlaceholders = true,
+  }
+}
 
 require('leap')
 vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
