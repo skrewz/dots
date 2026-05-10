a5ae9a9813519b839396e011cfcb4284104504e7
Anders Breindahl <skrewz@skrewz.net>
2026-05-10 19:54:07 +1000

    👷 feat: shift left Go related linting
    (Or, well, make a stab at it, anyway. Can't hurt.)

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -178,12 +178,38 @@ return {
         gopls = {
           settings = {
             gopls = {
+              gofumpt = false,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableType = true,
+              },
               experimentalPostfixCompletions = true,
               analyses = {
+                nilness = true,
                 unusedparams = true,
+                unusedwrite = true,
+                useany = true,
                 shadow = true,
               },
+              usePlaceholders = true,
+              completeUnimported = true,
               staticcheck = true,
+              directoryFilters = { '-.git', '-node_modules' },
+              semanticTokens = true,
             },
           },
           capabilities = require('cmp_nvim_lsp').default_capabilities(),
