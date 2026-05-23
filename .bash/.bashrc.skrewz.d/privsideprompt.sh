c84dbf9698c945efdd92410e6bceaa41dec90cd8
Anders Breindahl <skrewz@skrewz.net>
2026-05-23 22:36:18 +1000

    🔥 chore: get rid of a bunch of old bash stuff

--- .bash/.bashrc.skrewz.d/privsideprompt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-if hostname -s | md5sum | grep -qF $'773a2038b8bfe5ca064e8de03079e711\n33c4ca47bb83c1b193620757dd9a7231'; then
-  functions_to_render_prompt+=( render_no_ssh_profiles )
-fi
